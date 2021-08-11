45d11a129fb0b32f9178d8649e9b6e7717086582
Benjamin <benmneb@gmail.com>
2021-08-11 14:58:44 +0800

    clean up routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,16 @@
 export default function routes(app, db) {
+  const highScores = db.collection('highScores');
+
   app.get('/', (req, res) => {
     res.send('Hello World!!1 🤓');
   });
 
   app.get('/api', async (req, res) => {
     try {
-      const response = await db.collection('highScores').find().limit(5).toArray();
-      console.log('response:', response);
+      const response = await highScores.find().sort({ score: -1, date: 1 }).limit(5).toArray();
       res.status(200).json(response);
     } catch (error) {
+      res.status(500).json({ 'Error getting scores': error.message });
       console.error('Error getting scores:', error.message);
     }
   });
@@ -16,12 +18,11 @@ export default function routes(app, db) {
   app.post('/api', async (req, res) => {
     const { name, score } = req.body;
 
-    console.log('req.body:', req.body);
-
     try {
-      const result = await db.collection('highScores').insertOne({ name, score });
+      const result = await highScores.insertOne({ name, score, date: new Date() });
       res.status(200).json(result);
     } catch (error) {
+      res.status(500).json({ 'Error adding score': error.message });
       console.error('Error adding score:', error.message);
     }
   });
